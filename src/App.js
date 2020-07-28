f895173ee3c57e0cc5db433bff097dbebd12be85
Robson de Jesus <robson.djsilva@gmail.com>
2020-07-28 09:30:42 -0300

    Added styled header of application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+
 
 function App() {
   return (
     <div>
+      <Menu />
       <h1>Hello World</h1>
     </div>
   );
