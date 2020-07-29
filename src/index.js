01082607cbd3ba34ff505404f3857f4e8a31c3e2
Robson de Jesus <robson.djsilva@gmail.com>
2020-07-29 13:04:05 -0300

    Added components pages, links with react router dom and another adaptations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={() => (<div>Página não encontrada</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
